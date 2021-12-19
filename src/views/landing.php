00fbf66ebde16732f7e7870887832537d4cc0629
Thomas Cardon <thomas.cardon@pm.me>
2021-12-19 02:05:18 +0100

    (Missed this file in last commit)

--- src/views/landing.php
+++ src/views/landing.php
@@ -11,19 +11,40 @@
             <h6 class="text-gray-1 slide-in-bottom-subtitle">
                 Créez vos évènements en quelques clics, gérez vos invités, organisez sans inquiétude votre soirée.
             </h6>
-            <button class="slide-in-bottom" style="margin-top: 1rem">Inscrivez-vous</button>
+            <div class="button-group" style="margin-top: 1rem">
+                <?php if (!$params['authentified']) : ?>
+                    <button class="action slide-in-bottom" style="margin-top: 1rem">Inscrivez-vous</button>
+                    <button class="slide-in-bottom" style="margin-top: 1rem">Connectez-vous</button>
+                <?php else : ?>
+                    <button class="flex items-center px-4 py-2 text-sm font-medium sm text-green-6 green">
+                        <svg class="text-green-5" width="12" height="20" fill="currentColor">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M6 5a1 1 0 011 1v3h3a1 1 0 110 2H7v3a1 1 0 11-2 0v-3H2a1 1 0 110-2h3V6a1 1 0 011-1z"></path>
+                        </svg>
+                        Nouveau
+                    </button>
+                <?php endif; ?>
+            </div>
         </div>
         <div id="recentEvents" class="w-1/2 lead fade-in events-list text-gray-less">
             <header class="flex items-center justify-between">
-                <h6>Derniers événements</h6>
-                <button class="flex items-center px-4 py-2 text-sm font-medium sm text-green-6 green">
-                    <svg class="text-green-5" width="12" height="20" fill="currentColor">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6 5a1 1 0 011 1v3h3a1 1 0 110 2H7v3a1 1 0 11-2 0v-3H2a1 1 0 110-2h3V6a1 1 0 011-1z"></path>
-                    </svg>
-                    Nouveau
-                </button>
+                <div>
+                    <h6>Derniers événements en date</h6>
+                    <h7 class="text-gray-4">
+                        Campagne du 01/01: <i>Tournoi voleyball</i>
+                    </h7>
+                </div>
+                <?php if ($params['authentified']) : ?>
+                    <button class="flex items-center px-4 py-2 text-sm font-medium sm text-green-6 green">
+                        <svg class="text-green-5" width="12" height="20" fill="currentColor">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M6 5a1 1 0 011 1v3h3a1 1 0 110 2H7v3a1 1 0 11-2 0v-3H2a1 1 0 110-2h3V6a1 1 0 011-1z"></path>
+                        </svg>
+                        Nouveau
+                    </button>
+                <?php endif; ?>
             </header>
-            <input class="w-full green search text-gray-less" type="text" aria-label="Filtrer les idées d'évènements" placeholder="Filtrer les idées d'évènements" />
+            <!-- 
+                <input class="w-full green search text-gray-less" type="text" aria-label="Filtrer les idées d'évènements" placeholder="Filtrer les idées d'évènements" />
+            -->
         </div>
     </section>
     <?php View::show('components/footer', array( 'landing' => true )); ?>
