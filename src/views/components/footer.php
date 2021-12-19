7b1105b500ed7269f04d5f6d69475d3cf7144fc1
Thomas Cardon <thomas.cardon@pm.me>
2021-12-19 02:05:44 +0100

    Bourbon UI improvements

--- src/views/components/footer.php
+++ src/views/components/footer.php
@@ -1,5 +1,5 @@
 <footer class="<?php echo $params['landing'] ? 'text-center' : 'text-left' ?>">
-    <a class="text-gray-<?php echo $params['landing'] ? 1 : 5 ?>" href="#">
+    <a class="text-gray-<?php echo $params['landing'] ? 3 : 5 ?>" href="#">
         &copy; E-Event.IO -  <?php echo date('Y'); ?>
     </a>
 </footer>
