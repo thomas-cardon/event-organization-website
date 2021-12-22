ff557284a0ddcad7be9a7ad17e1be18c8752f454
Thomas Cardon <thomas.cardon@pm.me>
2021-12-22 16:28:16 +0100

    /event/see UI improvements

--- src/views/event/see.php
+++ src/views/event/see.php
@@ -4,7 +4,7 @@
     <section class="events">
         <div class="overview glass">
             <header class="flex items-center">
-                <h2 class="title">
+                <h2>
                     <?php echo $params['event']['title']; ?>
                 </h2>
                 <h3 class="author text-gray-1">
@@ -16,7 +16,9 @@
                 <?php echo $params['event']['description']; ?>
             </div>
 
-            <footer class="flex items-center">
+            <hr />
+
+            <footer class="text-gray-3">
                 <h3 class="date">
                     <?php echo $params['event']['date']; ?>
                 </h3>
@@ -30,15 +32,64 @@
         </div>
         <div class="user glass">
             <header class="flex items-center">
-                <img class="avatar" src="<?php echo $params['event']['owner']['avatar']; ?>" alt="Avatar de <?php echo $params['event']['owner']['firstName'] . $params['event']['owner']['lastName']; ?>">
-                <h1 class="title">
+                <div class="avatar-container">
+                    <img class="avatar" onerror="this.src='<?php echo Constants::getPublicPath() . '/vendor/svg/placeholder-bg.svg'; ?>'" src="<?php echo $params['event']['owner']['avatar']; ?>" alt="Avatar de <?php echo $params['event']['owner']['firstName'] . $params['event']['owner']['lastName']; ?>">
+                </div>
+                <h2>
                     <?php echo $params['event']['owner']['firstName'] . ' ' . $params['event']['owner']['lastName']; ?>
-                </h1>
+                </h2>
                 <h3 class="author text-gray-1">
                     <?php echo $params['event']['owner']['email']; ?>
                 </h3>
             </header>
         </div>
-        <div class="misc glass"></div>
+        <div class="misc glass">
+            <h2>Voir aussi</h2>
+            <div class="flex flex-wrap">
+                <?php 
+                    $params['related'] = array(
+                        array(
+                            'title' => 'Titre de l\'évènement',
+                            'description' => 'Description de l\'évènement',
+                            'date' => 'Date de l\'évènement',
+                            'time' => 'Heure de l\'évènement',
+                            'location' => 'Lieu de l\'évènement',
+                            'owner' => array(
+                                'firstName' => 'Prénom de l\'auteur',
+                                'lastName' => 'Nom de l\'auteur',
+                                'avatar' => 'Avatar de l\'auteur',
+                                'email' => 'Email de l\'auteur'
+                            )
+                        ),
+                        array(
+                            'title' => 'Titre de l\'évènement',
+                            'description' => 'Description de l\'évènement',
+                            'date' => 'Date de l\'évènement',
+                            'time' => 'Heure de l\'évènement',
+                            'location' => 'Lieu de l\'évènement',
+                            'owner' => array(
+                                'firstName' => 'Prénom de l\'auteur',
+                                'lastName' => 'Nom de l\'auteur',
+                                'avatar' => 'Avatar de l\'auteur',
+                                'email' => 'Email de l\'auteur'
+                            )
+                        ),
+                        array(
+                            'title' => 'Titre de l\'évènement',
+                            'description' => 'Description de l\'évènement',
+                            'date' => 'Date de l\'évènement',
+                            'time' => 'Heure de l\'évènement',
+                            'location' => 'Lieu de l\'évènement',
+                            'owner' => array(
+                                'firstName' => 'Prénom de l\'auteur',
+                                'lastName' => 'Nom de l\'auteur',
+                                'avatar' => 'Avatar de l\'auteur',
+                                'email' => 'Email de l\'auteur'
+                            )
+                        )
+                    );
+                    
+                    $params['related'] = array_slice($params['related'], 0, 5); ?>
+        </div>
     </section>
 </section>
