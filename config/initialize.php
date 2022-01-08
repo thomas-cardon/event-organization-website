f4e7ff10dbc664d98489fd7b431943398c8572b5
Thomas Cardon <thomas.cardon@pm.me>
2022-01-08 03:04:00 +0100

    Initialisation: suppression des foreign checks, insertion de fausses val

--- config/initialize.php
+++ config/initialize.php
@@ -15,11 +15,12 @@ if (file_exists($p . '/sql.php')) require_once $p . '/sql.php';
 else die('Le fichier de configuration de la base de données SQL n\'existe pas.');
 
 $sql = [
-
+    'SET FOREIGN_KEY_CHECKS = 0',
     'DROP TABLE IF EXISTS `transactions`',
     'DROP TABLE IF EXISTS `campaigns`',
     'DROP TABLE IF EXISTS `events`',
     'DROP TABLE IF EXISTS `users`',
+    'SET FOREIGN_KEY_CHECKS = 1',
     /* A partir d'ici, ce sont des exemples de requêtes générées automatiquement */
     'CREATE TABLE `users` (
         `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -43,16 +44,32 @@ $sql = [
         PRIMARY KEY (`id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8',
 
-
     'CREATE TABLE IF NOT EXISTS `events` (
         `id` int(11) NOT NULL AUTO_INCREMENT,
         `name` varchar(255) NOT NULL,
         `description` text NOT NULL,
+        `author` int(11) NOT NULL,
+        `status` varchar(255) NOT NULL DEFAULT \'pending\',
         `created_at` DATETIME DEFAULT NOW(),
         `updated_at` DATETIME DEFAULT NOW() ON UPDATE NOW(),
-        PRIMARY KEY (`id`)
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (`author`) REFERENCES `users`(`id`) ON DELETE CASCADE
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8',
-    /* Transactions, @author Thomas Cardon */
+        
+    'CREATE TABLE IF NOT EXISTS `events_unlockablecontent` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `name` varchar(255) NOT NULL,
+        `description` text NOT NULL,
+        `points_required` int(11) NOT NULL,
+        `event` int(11) NOT NULL,
+        `author` int(11) NOT NULL,
+        `created_at` DATETIME DEFAULT NOW(),
+        `updated_at` DATETIME DEFAULT NOW() ON UPDATE NOW(),
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (`event`) REFERENCES `events` (`id`),
+        FOREIGN KEY (`author`) REFERENCES `users` (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8',
+
     'CREATE TABLE `transactions` (
         `id` int(11) NOT NULL AUTO_INCREMENT,
         `user_id` int(11) NOT NULL,
@@ -68,15 +85,16 @@ $sql = [
 
 $sql_data = [
     "INSERT INTO `campaigns` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'la campagne', 'la description', '2021-12-29 11:52:28', '2021-12-29 12:52:28');",
-    "INSERT INTO `events` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'event1', 'la description', '2021-12-29 11:40:36', '2021-12-29 12:51:19');",
+        (1, 'la campagne', 'la description', '2021-12-29 11:52:28', '2021-12-29 12:52:28');",
+    
     "INSERT INTO `users` (`id`, `first_name`, `last_name`, `hash`, `email`, `role`, `created_at`, `updated_at`) VALUES
-(1, 'Jane', 'Doe', '$2y$10\$ecbqAqsHQZ.xXVzCN93P5ucVv7J4vUlNDeCZ315HsxLzPdaYwXsMC', 'test.test@test.fr', 'admin', '2021-12-29 12:08:25', '2021-12-29 12:08:54'),
-(2, 'Thor', 'Odinson', '$2y$10\$ecbqAqsHQZ.xXVzCN93P5ucVv7J4vUlNDeCZ315HsxLzPdaYwXsMC', 'thomas.cardon@etu.univ-amu.fr', 'organizer', '2021-12-29 12:08:25', '2021-12-29 12:08:54'),
-(3, 'John', 'Doe', '$2y$10\$ecbqAqsHQZ.xXVzCN93P5ucVv7J4vUlNDeCZ315HsxLzPdaYwXsMC', 'test1.test2@test.fr', 'member', '2021-12-29 12:08:25', '2021-12-29 12:08:54');",
-    "INSERT INTO `transactions` (`id`, `user_id`, `event_id`, `amount`, `created_at`, `comment`) VALUES
-(2, 1, 1, 3333, '2021-12-29 12:41:48', 'Le commentaire');"];
+        (1, 'Jane', 'Doe', '$2y$10\$ecbqAqsHQZ.xXVzCN93P5ucVv7J4vUlNDeCZ315HsxLzPdaYwXsMC', 'test.test@test.fr', 'admin', '2021-12-29 12:08:25', '2021-12-29 12:08:54'),
+        (2, 'Thor', 'Odinson', '$2y$10\$ecbqAqsHQZ.xXVzCN93P5ucVv7J4vUlNDeCZ315HsxLzPdaYwXsMC', 'thomas.cardon@etu.univ-amu.fr', 'organizer', '2021-12-29 12:08:25', '2021-12-29 12:08:54'),
+        (3, 'John', 'Doe', '$2y$10\$ecbqAqsHQZ.xXVzCN93P5ucVv7J4vUlNDeCZ315HsxLzPdaYwXsMC', 'test1.test2@test.fr', 'member', '2021-12-29 12:08:25', '2021-12-29 12:08:54');",
+    
+    "INSERT INTO `events` (`id`, `name`, `author`, `description`, `created_at`, `updated_at`) VALUES (2, 'event2', 1, 'la description', '2021-12-29 11:40:36', '2021-12-29 12:51:19');",
+    "INSERT INTO `transactions` (`id`, `user_id`, `event_id`, `amount`, `created_at`, `comment`) VALUES (1, 1, 2, 10, '2021-12-29 11:40:36', 'comment');"
+];
 
 $db = Model::getDatabaseInstance();
 
