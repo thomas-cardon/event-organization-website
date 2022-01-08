30fcb80b129eb7d21bd89b00ac4d64c423cefabc
Thomas Cardon <thomas.cardon@pm.me>
2022-01-08 02:48:19 +0100

    Favicon

--- src/views/_layout/document.php
+++ src/views/_layout/document.php
@@ -6,7 +6,7 @@
 
         <title>E-Event.IO | <?php echo View::getTitle() ?></title>
 
-        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
+        <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22%23000000%22></rect><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2282%22>🎫</text></svg>" />
         <link rel="preload" href="<?php echo Constants::getPublicPath() ?>/vendor/css/bourbon/font/BigNoodleTooOblique.woff2" as="font" type="font/woff2" crossorigin>
 
         <link rel="stylesheet" href="<?php echo Constants::getPublicPath() ?>/vendor/css/normalize.css" />
