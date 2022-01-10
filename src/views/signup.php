b7105899371c09d31fb4d55ce39f6f672af4b0ad
Thomas Cardon <thomas.cardon@pm.me>
2022-01-11 00:28:10 +0100

    /sign-up UI fixed

--- src/views/signup.php
+++ src/views/signup.php
@@ -1,6 +1,6 @@
 <?php View::setTitle('Inscription'); ?>
 
-<main class="lead">
+<main>
     <?php View::show('components/header', $params); ?>
 
     <?php if (isset($params['alert'])) : ?>
@@ -9,20 +9,31 @@
         </div>
     <?php endif; ?>
 
-    <section class="p-2">
-        <h1 class="slide-in-bottom-h1">Inscription</h1>
-        <h6 class="text-gray-1 slide-in-bottom-subtitle subtitle">
-            Rejoignez la communauté et découvrez les évènements de votre région.
-        </h6>
-        
-        <form action="<?= BASE_PATH . '/signup'; ?>" method="POST">
-            <div class="input-group horizontal">
-                <input class="w-full glass text-gray-less" type="text" name="nom" id="nom" aria-label="Votre nom" placeholder="Votre nom" required />
-                <input class="w-full glass text-gray-less" type="text" name="prenom" id="prenom" aria-label="Votre prénom" placeholder="Votre prenom" required />
-                <input class="w-full glass text-gray-less" type="email" name="email" id="email" aria-label="Votre adresse e-mail" placeholder="Votre adresse e-mail" required />
+    <section class="hero">
+        <section class="p-2 lead" style="margin: auto auto !important;width: 50rem;">
+            <div class="desc">
+                <h1 class="font-hero slide-in-bottom-h1">Inscription</h1>
+                <p class="text-gray-1 slide-in-bottom-subtitle">
+                    Rejoignez la communauté et découvrez les évènements de votre région.
+                </p>
             </div>
-            <input type="submit" class="action slide-in-bottom" style="margin-top: 1rem" value="Créer" />
-        </form>
+            <form action="<?= BASE_PATH . '/signup/post'; ?>" method="POST">
+                <div class="input-group">
+                    <input class="glass text-gray-less" type="text" name="firstName" id="firstName" aria-label="Votre prénom" placeholder="Votre prénom" required />
+                    <input class="glass text-gray-less" type="text" name="lastName" id="lastName" aria-label="Votre nom" placeholder="Votre nom" required />
+                </div>
+
+                <div class="input-group horizontal">
+                    <input class="w-full glass text-gray-less" type="email" name="email" id="email" aria-label="Votre adresse e-mail" placeholder="Votre adresse e-mail" required />
+                    <input class="w-full glass text-gray-less" type="password" name="password" id="password" aria-label="Votre mot de passe" placeholder="Votre mot de passe" required />
+                </div>
+
+                <div class="horizontal justify-start" style="gap: 1rem">
+                    <input type="submit" class="btn action slide-in-bottom" value="S'inscrire" />
+                </div>
+            </form>
+        </section>
     </section>
+
     <?php View::show('components/footer', array( 'landing' => true )); ?>
 </main>
