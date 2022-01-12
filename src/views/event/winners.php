4a54c9cbce0dda53f4fc3fb72b164eb64491ee4c
Thomas Cardon <thomas.cardon@pm.me>
2022-01-12 23:16:57 +0100

    /event/winners UI improvements

--- src/views/event/winners.php
+++ src/views/event/winners.php
@@ -4,26 +4,30 @@
 
 <section class="lead">
     <h1 class="font-hero slide-in-bottom-h1">Grands gagnants</h1>
-    <div class="glass fade-in">
-        <table>
+    <div class="glass fade-in" style="margin-top: 2rem">
+        <table class="w-full">
             <thead>
                 <tr>
-                    <th>Nom de l'évènement</th>
+                    <th>Nom</th>
                     <th>Description</th>
-                    <th>Date de début</th>
-                    <th>Date de fin</th>
+                    <th>Début</th>
+                    <th>Fin</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($params['winners'] as $winner) { ?>
+                <?php foreach ($params['events'] as $event) { ?>
                 <tr>
-                    <td><?php echo $winner['name']; ?></td>
-                    <td><?php echo $winner['description']; ?></td>
-                    <td><?php echo $winner['start_date']; ?></td>
-                    <td><?php echo $winner['end_date']; ?></td>
+                    <td><?= $event->getName(); ?></td>
+                    <td class="xs"><?= $event->getDescription(); ?></td>
+                    <td><?= $event->getFrom()->format('d/m'); ?></td>
+                    <td><?= $event->getTo()->format('d/m'); ?></td>
                     <td>
-                        <a href="<?php echo BASE_PATH . '/event/winner/' . $winner['id']; ?>">
-                            <i class="fas fa-eye"></i>
+                        <a href="<?php echo BASE_PATH . '/event/see/' . $event->getId(); ?>">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                            </svg>
                         </a>
                     </td>
                 </tr>
