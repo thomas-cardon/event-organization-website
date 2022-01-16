a3ebeec9f56cadc68a61cdae6dbf0d997a924730
Thomas Cardon <thomas.cardon@pm.me>
2022-01-16 22:50:45 +0100

    Config file update

--- config/config.example.php
+++ config/config.example.php
@@ -9,3 +9,18 @@ define('BASE_PATH', '/[insérer url de votre projet]');
 points pour être considérée (sinon elle n’apparaitra pas dans la liste des choix du jury, en fin
 de campagne)" */
 define('EVENT_MIN_POINTS_REQUIRED', 1000);
+
+/*
+ * Donne x points à un donateur au début de chaque campagne
+ */
+define('DONOR_POINTS_GIVEN_FOR_EACH_CAMPAIGN', 3000);
+
+/*
+ * Donne x points à un donateur lorsqu'il s'inscrit
+ */
+define('DONOR_POINTS_GIVEN_AT_REGISTRATION', 10000);
+
+/*
+ * Passe outre la règle au dessus, et répartit les points donné par l'API REST entre les donateurs
+*/
+define('POINTS_WEBSERVICE_URL', BASE_URL . '/webservices/points-bank-rest-api/get.php');
