83bb233a59d7dfe8c622ba6db8a6c1fb47be75f9
Justin De Sio <65298664+Justin-De-Sio@users.noreply.github.com>
2022-01-16 09:04:36 +0100

    Update sql.example.php

--- config/sql.example.php
+++ config/sql.example.php
@@ -1,8 +1,10 @@
 <?php
 
 const DB_HOST = 'localhost';
-const DB_NAME = 'e-event.io';
-const DB_USER = 'root';
-const DB_PASS = '';
+const DB_NAME = 'name_here';
+const DB_USER = 'username_here';
+const DB_PASS = 'password_here';
 const DB_CHARSET = 'utf8';
 const DB_COLLATE = '';
+<?php
+
