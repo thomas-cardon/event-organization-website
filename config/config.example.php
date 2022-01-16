886a0a27d873ce5176631f1779b41baee26e29f4
Justin-De-Sio <desio.j@live.fr>
2022-01-16 22:50:47 +0100

    constante DONOR_POINTS_GIVEN_FOR_EACH_CAMPAIGN rajouter en concaténation

--- config/config.example.php
+++ config/config.example.php
@@ -3,9 +3,10 @@
 setlocale(LC_TIME, 'fr_FR');
 date_default_timezone_set('Europe/Paris');
 
-define('BASE_PATH', '/[insérer url de votre projet]');
+const BASE_PATH = '/[insérer url de votre projet]';
 
 /* "L’idée d’événement a un coût minimum de
 points pour être considérée (sinon elle n’apparaitra pas dans la liste des choix du jury, en fin
 de campagne)" */
-define('EVENT_MIN_POINTS_REQUIRED', 1000);
+const EVENT_MIN_POINTS_REQUIRED = 10000;
+const  DONOR_POINTS_GIVEN_FOR_EACH_CAMPAIGN = 500;
