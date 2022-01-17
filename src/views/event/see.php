7872c8766336d98e728cca339f156c19c0d44eb4
Justin-De-Sio <desio.j@live.fr>
2022-01-17 15:25:55 +0100

    <?=

--- src/views/event/see.php
+++ src/views/event/see.php
@@ -8,15 +8,15 @@
         <div class="overview glass slide-in-bottom">
             <header class="flex items-center">
                 <h2 class="font-hero">
-                    <?php echo $params['event']->getName(); ?>
+                    <?= $params['event']->getName(); ?>
                 </h2>
                 <p class="m text-gray-1">
-                    <i>par <?php echo $author->getName(); ?></i>
+                    <i>par <?= $author->getName(); ?></i>
                 </p>
             </header>
 
             <div class="description">
-                <?php echo $params['event']->getDescription(); ?>
+                <?= $params['event']->getDescription(); ?>
             </div>
 
             <hr />
@@ -27,17 +27,17 @@
                 <div class="unlockable-content-container">
                     <?php
                         foreach ($unlockable as $u) { ?>
-                            <div class="glass gold unlockable-content locked" style="background-image: url(<?php echo $content->getImage(); ?>);color: <?php echo $content->getTextColor() ?? 'white'; ?>">
+                            <div class="glass gold unlockable-content locked" style="background-image: url(<?= $content->getImage(); ?>);color: <?= $content->getTextColor() ?? 'white'; ?>">
                                 <h4 class="font-hero">
-                                    <?php echo $u->getTitle(); ?>
+                                    <?= $u->getTitle(); ?>
                                 </h4>
                                 <p class="desc">
-                                    <?php echo $u->getDescription(); ?>
+                                    <?= $u->getDescription(); ?>
                                 </p>
                                 
                                 <?php if ($u->getRequiredPoints() > $params['event']->getPoints()): ?>
                                     <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg>
-                                    <span><?php echo $params['event']->getPoints() ?>/<?php echo $u->getRequiredPoints(); ?></span>
+                                    <span><?= $params['event']->getPoints() ?>/<?= $u->getRequiredPoints(); ?></span>
                                 <?php else: ?>
                                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z"></path></svg>
                                     <span>Contenu débloqué !</span>
@@ -54,15 +54,15 @@
 
             <footer class="text-gray-3">
                 <h5 class="date meta s">
-                    Du <?php echo $params['event']->getStartDate()->format('Y/m/d à H:i'); ?>
-                    au <?php echo $params['event']->getEndDate()->format('Y/m/d à H:i'); ?>
+                    Du <?= $params['event']->getStartDate()->format('Y/m/d à H:i'); ?>
+                    au <?= $params['event']->getEndDate()->format('Y/m/d à H:i'); ?>
                 </p>
                 <h5 class="points meta s">
                     <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd">
                         </path>
                     </svg>
-                    <?php echo $params['event']->getPointsAmount() ?>
+                    <?= $params['event']->getPointsAmount() ?>
                 </h5>
             </footer>
         </div>
@@ -70,13 +70,13 @@
             <div class="user glass slide-in-bottom">
                 <header class="flex items-center">
                     <div class="avatar-container">
-                        <img class="avatar" onerror="this.src='<?php echo Constants::getPublicPath() . '/vendor/svg/placeholder-bg.svg'; ?>'" src="<?php echo $author->getAvatar(); ?>" alt="Avatar de <?php echo $author->getName(); ?>">
+                        <img class="avatar" onerror="this.src='<?= Constants::getPublicPath() . '/vendor/svg/placeholder-bg.svg'; ?>'" src="<?= $author->getAvatar(); ?>" alt="Avatar de <?= $author->getName(); ?>">
                     </div>
                     <h2 class="font-hero slide-in-bottom-h1">
-                        <?php echo $author->getName(); ?>
+                        <?= $author->getName(); ?>
                     </h2>
                     <p class="m text-gray-1 slide-in-bottom-subtitle">
-                        <?php echo $author->getEmail(); ?>
+                        <?= $author->getEmail(); ?>
                     </p>
                 </header>
             </div>
@@ -92,7 +92,7 @@
                 <?php if (empty($params['donations'])): ?>
                     <p>
                         Aucun don n'a été effectué pour l'instant. &nbsp;
-                        <a href="<?php echo BASE_PATH . '/event/donate/' . $params['event']->getId(); ?>">
+                        <a href="<?= BASE_PATH . '/event/donate/' . $params['event']->getId(); ?>">
                             Donner des points
                         </a>
                     </p>
@@ -109,17 +109,17 @@
                             <?php foreach ($params['donations'] as $t): ?>
                                 <tr>
                                     <td>
-                                        <?php echo $t->getUser()->getName(); ?>
+                                        <?= $t->getUser()->getName(); ?>
                                     </td>
                                     <td>
-                                        <?php echo $t->getCreatedAt()->format('d/m à H:i'); ?>
+                                        <?= $t->getCreatedAt()->format('d/m à H:i'); ?>
                                     </td>
                                     <td>
                                         <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd">
                                             </path>
                                         </svg>
-                                        <?php echo $t->getAmount(); ?>
+                                        <?= $t->getAmount(); ?>
                                     </td>
                                 </tr>
                             <?php endforeach; ?>
--- src/views/event/winners.php
+++ src/views/event/winners.php
@@ -23,7 +23,7 @@
                     <td><?= $event->getStartDate()->format('d/m'); ?></td>
                     <td><?= $event->getEndDate()->format('d/m'); ?></td>
                     <td>
-                        <a href="<?php echo BASE_PATH . '/event/see/' . $event->getId(); ?>">
+                        <a href="<?= BASE_PATH . '/event/see/' . $event->getId(); ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
