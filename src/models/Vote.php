ebc99d462b6e9beeebb467b8e71178938c0cae5d
Justin-De-Sio <desio.j@live.fr>
2022-01-18 09:54:54 +0100

    ajout $id car c'est de toute façon créer par default par la bd autant représenter id ici aussi

--- src/models/Vote.php
+++ src/models/Vote.php
@@ -2,13 +2,15 @@
 
 final class Vote extends Model
 {
+    private $id;
     private $eventId;
     private $userId;
 
-    public function __construct($eventId, $userId)
+    public function __construct($eventId, $userId, $id = null)
     {
         $this->eventId = $eventId;
         $this->userId = $userId;
+        $this->id = $id;
     }
 
     public static function hasVoted($eventId, $userId): bool
@@ -20,24 +22,6 @@ final class Vote extends Model
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         return $row !== false;
-    }
-
-    public function save(): bool
-    {
-        $sql = 'INSERT INTO votes (event_id, user_id) VALUES (:event_id, :user_id)';
-        $stmt = self::getDatabaseInstance()->prepare($sql);
-        $stmt->bindParam(':event_id', $this->eventId, PDO::PARAM_INT);
-        $stmt->bindParam(':user_id', $this->userId, PDO::PARAM_INT);
-        return $stmt->execute();
-    }
-
-    public function delete(): bool
-    {
-        $sql = 'DELETE FROM votes WHERE event_id = :event_id AND user_id = :user_id';
-        $stmt = self::getDatabaseInstance()->prepare($sql);
-        $stmt->bindParam(':event_id', $this->eventId, PDO::PARAM_INT);
-        $stmt->bindParam(':user_id', $this->userId, PDO::PARAM_INT);
-        return $stmt->execute();
     }
 
     public static function getVotesForEvent($eventId): array
@@ -57,3 +41,22 @@ final class Vote extends Model
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function save(): bool
+    {
+        $sql = 'INSERT INTO votes (event_id, user_id) VALUES (:event_id, :user_id)';
+        $stmt = self::getDatabaseInstance()->prepare($sql);
+        $stmt->bindParam(':event_id', $this->eventId, PDO::PARAM_INT);
+        $stmt->bindParam(':user_id', $this->userId, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public function delete(): bool
+    {
+        $sql = 'DELETE FROM votes WHERE event_id = :event_id AND user_id = :user_id';
+        $stmt = self::getDatabaseInstance()->prepare($sql);
+        $stmt->bindParam(':event_id', $this->eventId, PDO::PARAM_INT);
+        $stmt->bindParam(':user_id', $this->userId, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+}
